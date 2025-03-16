5c1ea3d9ef6e42f6482340d514cc014e4a699304
raquel-manajreh <raquel.manajreh@bootcamp-upgrade.com>
2025-03-16 17:50:04 +0100

    componente de contacto con css actualizado

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,26 +3,34 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <form action="">
-    <div>
-        <label htmlFor="name">Nombre</label>
-        <input type="text" name="name" id="name"/>
+    <>
+    <div className="bodyContact">
+      <h2>CONTACTO</h2>
+
+      <section  className="cardContact">
+
+        <form className="formContact" action="">
+
+        <div className="divLabel">
+            <label htmlFor="name">NOMBRE</label>
+            <input type="text" name="name" id="name"/>
+        </div>
+
+        <div className="divLabel">
+            <label htmlFor="email">E-MAIL</label>
+            <input type="text" name="email" id="email"/>
+        </div>
+      
+        <div className="divLabel">
+        <label htmlFor="message">MENSAJE</label>
+
+        <input className="messageInput" type="text" name="message" id="message"/>
+        </div>
+            <button className="sendButton" >Enviar</button>
+        </form>
+      </section>
     </div>
-
-    <div>
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email"/>
-    </div>
-        
-    <div>
-    <label htmlFor="message">Mensaje</label>
-    <input type="text" name="message" id="message"/>
-    </div>
-
-        <button>Enviar</button>
-
-
-    </form>
+   </>
   )
 }
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,6 @@ function App() {
 
   return (
     <div>
-      <h1>Mi Portfolio</h1>
       <NavBar setCurrentPage={setCurrentPage} />
       <main>
         { currentPage === "contact" ? <Contact /> :  
