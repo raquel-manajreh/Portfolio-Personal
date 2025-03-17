cb59e6fa3deb5ddb67c84220e14cbe5c37cd8e10
raquel-manajreh <raquel.manajreh@bootcamp-upgrade.com>
2025-03-17 21:59:24 +0100

    algunos fallos de css corregidos

--- src/Pages/Home/Experience/Experience.jsx
+++ src/Pages/Home/Experience/Experience.jsx
@@ -3,12 +3,12 @@ import "./Experience.css";
 function Experience({data}) {
   return (
     <div>
-        <div className="computerCard">
+        <div className="computerCard experienceCard">
             {data.map((item, i)=><article key={i}>
                <p className="workstation">🛠️ {item.name}</p>
                <p className="workdate">🗓️ {item.date}</p>
                <p className="job">💻 {item.where}</p>
-               <p className="workdescription">🫱🏼‍🫲🏼 {item.description}</p>
+               <p className="workDescription">🫱🏼‍🫲🏼 {item.description}</p>
 
             </article>)}
         </div>
